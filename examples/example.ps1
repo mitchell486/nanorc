9f0a3c6ea6c1a476e5f3df26a4f8e4ff0a524f2e
mitchell486 <mitchell.bumgarner@gmail.com>
2015-11-13 17:43:52 -0600

    Bat done, PS in middle of the comments section. -MB

--- examples/example.ps1
+++ examples/example.ps1
@@ -1,60 +1,22 @@
-@ echo off
-rem This is a test to find this
-a;ldsjfla;skdjf
-a;lsdjf;lasjfdlkajsdf
-a;sldjf;lakjsdflkjsadf
-remAnd this
-asrem This is a test too
-as;ldjfa;lsdjflajsf
-a;lsdkjf;lakjdsflkjadsf
-a;ldsjf;laksjdf
-test.sh
-tester.bat
-test.cmd
-test.cmds
-test.batch
-#!/bin/bash
-test
-var='rest'
-If %var% == 0
-"this is"
-"THIS IS"
-'this is a'
-'THIS IS A'
-{this is a test}
-{THIS IS A TEST}
-[test this too]
-[TEST THIS TOO]
-$variables
-$VARIABLES
-${variable}
-${VARIABLE}
-$@
-$VARIABLE_TESTING as well
-(testing)
-(TESTING)
-1VARIABLE(THIS)
-1(MATCH)
-abc()
-ABC()
-f%%a
-%%A
-call :function test
-CALL function.cmd 10 first
-CALL :sub_display 123
-test call :function test
+$var (should match)
+$var1 (should match)
+ $var1 (should match)
+ $var (should match)
+ $$var (should not match)
+$$var (should not match)
+$1var (should match)
+$_var (should match)
+$_var1 (should match)
+$_1var (should match)
+word$var$var2 (should match)
+# This is a comment
+ # This is a comment
+  # This is a comment
+	# This is a comment
+		# This is a comment
+#This is a comment
+test #This is a comment too.
+<#This is a comment
+that should continue #>
 
-:: test comment
-call:functionTest :: test comment #2
-	:: test comment #3
-"C:\Users\mitch\"
-%a
-if %var% EQU 1
-if %var% equ 2
-if %var% equals 1
-do this 	blah (
-	blah
-			)
-else (
-		blah2
-	)
+
