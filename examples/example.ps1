f99d4fb2bbd343c9fee22125b62460edb8ab76be
mitchell486 <mitchell.bumgarner@gmail.com>
2015-11-13 22:01:13 -0600

    Added a second line for finding all comment blocks on powershell.nanorc. -MB

--- examples/example.ps1
+++ examples/example.ps1
@@ -17,7 +17,9 @@ word$var$var2 (should match)
 #This is a comment
 test #This is a comment too.
 <#This is a comment
+this is totally a comment too
 that should continue #>
+this is not a comment #>
 "This is a quote"
 'This is also a quote'
 "This is not a real quote'
